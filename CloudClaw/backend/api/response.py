e35000d5f0e866c1cdbf76138d45d0f894e4cc84
mzx <tomkafu666@qq.com>
2026-03-04 20:33:52 +0800

    Delete CloudClaw directory

--- CloudClaw/backend/api/response.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Generic, TypeVar, Optional
-
-T = TypeVar("T")
-
-class ApiResponse(BaseModel, Generic[T]):
-    code: int = 200  # 200=成功，非0=失败
-    msg: str = "success"  # 中文提示信息
-    data: Optional[T] = None  # 返回数据
